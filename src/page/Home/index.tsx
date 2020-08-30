eb3ec3122555be0df94f3c12d3fdc9f2efd46c67
Thiago William <thiago.will.silva@gmail.com>
2020-08-29 23:07:43 -0300

    creating page home

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -4,12 +4,36 @@ import * as S from './styles';
 import SearchAppointment from '../../components/SearchAppointment';
 import Newsletter from '../../components/Newsletter';
 
+import SeloBlack from '../../assets/selo.png';
+
 const Home: React.FC = () => {
   return (
     <>
       <SearchAppointment />
       <S.Container>
-        <h1>Page Home</h1>
+        <h2>Assinaturas Doutor Hoje</h2>
+        <S.CardType>
+          <img src={SeloBlack} alt="Doutor Hoje Black" />
+          <S.BoxCard>
+            <S.TitleCard>Doutor Hoje Black</S.TitleCard>
+            <S.SubTitleCard>
+              <p>Consultas por R$29,50</p>
+              <span>em 41 especialidades médicas</span>
+            </S.SubTitleCard>
+            <S.DescriptionCard>
+              <p>Exames laboratoriais por R$5 e R$10</p>
+              <p>Consulta Online ilimitadas 24hrs</p>
+              <S.DefaultLink href="/"> Ver detalhes </S.DefaultLink>
+              <p>por apenas</p>
+              <p>
+                <strong>R$59,50</strong> mês por pessoa
+              </p>
+              <S.BlueLink href="/">Quero Assinar</S.BlueLink>
+            </S.DescriptionCard>
+
+            <S.DefaultLink href="/"> ver detalhes comparativa </S.DefaultLink>
+          </S.BoxCard>
+        </S.CardType>
       </S.Container>
       <Newsletter />
     </>
--- src/page/Home/styles.ts
+++ src/page/Home/styles.ts
@@ -1,3 +1,88 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.section`
+  width: 100%;
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 1.8rem 0;
+
+  h2 {
+    width: 100%;
+    text-align: center;
+    color: var(--color-strong-black);
+    font-weight: 400;
+  }
+`;
+export const CardType = styled.div`
+  width: 33%;
+  margin: 0 auto;
+  text-align: center;
+  img {
+    width: 150px;
+  }
+`;
+export const BoxCard = styled.article`
+  position: relative;
+`;
+export const TitleCard = styled.div`
+  background: var(--color-strong-black);
+  color: var(--color-background);
+  font-weight: 700;
+  font-size: 2rem;
+  text-align: left;
+  padding: 1.2rem 2rem;
+  border-radius: 0.8rem 0.8rem 0 0;
+`;
+export const SubTitleCard = styled.div`
+  background: var(--color-weak-black);
+  color: var(--color-background);
+  font-size: 1.6rem;
+  text-align: left;
+  padding: 1.2rem 2rem;
+  border-top: 0.5px solid #4f4f4f;
+`;
+export const DescriptionCard = styled.div`
+  border: 0.5px solid #c5c5c5;
+  padding: 1.2rem 2rem;
+  border-radius: 0 0 0.8rem 0.8rem;
+  p {
+    text-align: left;
+    width: 100%;
+    font-size: 1.4rem;
+  }
+`;
+const LinkDefault = css`
+  text-decoration: none;
+  transition: all 0.2s;
+`;
+
+export const WrapperLinks = styled.div``;
+
+export const DefaultLink = styled.a`
+  ${LinkDefault}
+  color: var(--color-link);
+  margin-right: 2rem;
+  font-size: 1.4rem;
+  text-align: left;
+  display: block;
+  margin: 1.6rem 0;
+  &:hover {
+    color: ${shade(0.2, '#0967ff')};
+  }
+`;
+
+export const BlueLink = styled.a`
+  ${LinkDefault}
+  font-size: 1.6rem;
+  text-transform: uppercase;
+  background: var(--color-link-background);
+  color: var(--color-link-with-background);
+  border-radius: 2.4rem;
+  padding: 0.8rem 2.4rem;
+  margin: 1.5rem 0;
+  display: inline-block;
+  &:hover {
+    background: ${shade(0.2, '#0967ff')};
+  }
+`;

a776c06c6fd4284e591ae483828832d359b386c8
Thiago William <thiago.will.silva@gmail.com>
2020-08-30 19:03:42 -0300

    finished footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,9 +4,105 @@ import * as S from './styles';
 
 const Footer: React.FC = () => {
   return (
-    <S.Container>
-      <h1>Footer</h1>
-    </S.Container>
+    <S.BlackBackground>
+      <S.Container>
+        <S.WrapperLinks>
+          <S.BoxLinks>
+            <strong>Central de atendimento</strong>
+            <S.ListBackground>
+              <li>0880 727 3620</li>
+            </S.ListBackground>
+
+            <strong>Vendas</strong>
+            <S.ListBackground>
+              <li>
+                <span>Brasília</span>
+                <span>(61) 4042-6525</span>
+              </li>
+
+              <li>
+                <span>
+                  <S.IconWhatsap />
+                </span>
+                <span>(61) 4042-6525</span>
+              </li>
+
+              <li>
+                <span>São Paulo</span>
+                <span>(11) 98450-1703</span>
+              </li>
+
+              <li>
+                <span>Outras Localidades</span>
+                <span>0800 727 3620</span>
+              </li>
+            </S.ListBackground>
+            <p>
+              Atendimento de segunda a sexta, das 8hrs às 18hrs, exceto feriados
+            </p>
+          </S.BoxLinks>
+
+          <S.BoxLinks>
+            <strong>Sobre nós</strong>
+            <ul>
+              <li>
+                <a href="/">Como a Doutor Hoje Funciona</a>
+              </li>
+              <li>
+                <a href="/">Perguntas Frequentes</a>
+              </li>
+              <li>
+                <a href="/">Nossos serviços</a>
+              </li>
+              <li>
+                <a href="/">Nossas rede</a>
+              </li>
+              <li>
+                <a href="/">Termos de Uso & política de Privacidade </a>
+              </li>
+              <li>
+                <a href="/">Aplicativo Doutor Hoje</a>
+              </li>
+            </ul>
+          </S.BoxLinks>
+
+          <S.BoxLinks>
+            <strong>Acessos Internos</strong>
+            <ul>
+              <li>
+                <a href="/">Administrativo</a>
+              </li>
+              <li>
+                <a href="/">Consultor</a>
+              </li>
+              <li>
+                <a href="/">Empresarial</a>
+              </li>
+              <li>
+                <a href="/">Prestador</a>
+              </li>
+              <li>
+                <a href="/">Paciente</a>
+              </li>
+            </ul>
+          </S.BoxLinks>
+        </S.WrapperLinks>
+        <S.WrapperIcons>
+          <a href="/">
+            <S.IconFacebook />
+          </a>
+          <a href="/">
+            <S.IconLinkedin />
+          </a>
+          <a href="/">
+            <S.IconInstagran />
+          </a>
+          <a href="/">
+            <S.IconYoutube />
+          </a>
+        </S.WrapperIcons>
+      </S.Container>
+    </S.BlackBackground>
   );
 };
 
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,107 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaInstagram,
+  FaYoutube,
+  FaWhatsapp,
+} from 'react-icons/fa';
 
-export const Container = styled.div``;
+export const BlackBackground = styled.section`
+  background: var(--color-strong-black);
+`;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 1.8rem 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const WrapperLinks = styled.div`
+  display: flex;
+`;
+export const BoxLinks = styled.div`
+  color: var(--color-white);
+  font-size: 1.2rem;
+  & + div {
+    margin-left: 3.2rem;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.8rem;
+  }
+
+  strong {
+    font-weight: 700;
+    font-size: 1.5rem;
+    margin-bottom: 0.8rem;
+    display: block;
+  }
+
+  a {
+    color: var(--color-white);
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const ListBackground = styled.ul`
+  li {
+    background: var(--color-weak-black);
+    padding: 0.8rem 1.8rem;
+    border-radius: 2.4rem;
+  }
+`;
+export const IconWhatsap = styled(FaWhatsapp)`
+  font-size: 1.8rem;
+`;
+
+export const WrapperIcons = styled.div`
+  display: flex;
+
+  a {
+    background: var(--color-white);
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    margin-left: 0.8rem;
+    transition: all 0.2s;
+    &:hover {
+      background: ${shade(0.1, '#fff')};
+      transform: scale(1.1);
+    }
+  }
+`;
+
+export const IconCss = css`
+  font-size: 1.8rem;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const IconFacebook = styled(FaFacebookF)`
+  ${IconCss}
+  fill: #3b5999;
+`;
+export const IconLinkedin = styled(FaLinkedinIn)`
+  ${IconCss}
+  fill: #0077B5;
+`;
+export const IconInstagran = styled(FaInstagram)`
+  ${IconCss}
+  fill: #e4405f;
+`;
+export const IconYoutube = styled(FaYoutube)`
+  ${IconCss}
+  fill: #cd201f;
+`;
